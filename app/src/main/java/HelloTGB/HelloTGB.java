9fe2bf973a74e0d7953ed26a4258dbddb79a8c28
BladeRunner2006 <2203454@jeffcoschools.us>
2019-11-22 16:45:53 -0700

    Hello TGB

---
+++ app/src/main/java/HelloTGB/HelloTGB.java
@@ -0,0 +1,12 @@
+package HelloTGB;
+
+public class HelloTGB {
+
+  public static void main(String[] args) {
+  
+  System.out.println("Hello Thanksgiving Break!!!");
+
+//  /storage/emulated/0/JavaNIDE/HelloTGB
+  }
+
+}
